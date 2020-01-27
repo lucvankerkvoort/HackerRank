3c5d4d2c560dddef34aa2532143c62367fe0ab4d
Luc van Kerkvoort <luc.van.kerkvoort@gmail.com>
2020-01-27 14:50:44 -0800

    finished the birthday cake solution

--- BirthdayCake/index.js
+++ BirthdayCake/index.js
@@ -1,8 +1,16 @@
 const arr = [3, 2, 1, 3];
 
 function birthdayCake(arr) {
-  const sorted = arr.sort((a, b) => a - b);
-  console.log(sorted);
+  let counter = 0;
+
+  const highestNum = Math.max(...arr);
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === highestNum) {
+      counter++;
+    }
+  }
+  console.log(counter);
 }
 
 birthdayCake(arr);
